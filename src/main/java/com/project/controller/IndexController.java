97522058aec69bb93bd65ddc9c386a804053bf21
Claire7734 <zr7734@gmail.com>
2017-11-25 22:07:53 +0800

    3rd class

--- src/main/java/com/project/controller/IndexController.java
+++ src/main/java/com/project/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Claire on 11/25/17.
@@ -11,15 +16,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("value1", 1);
+        List<String> colors = Arrays.asList(new String[]{"red", "blue", "green"});
+        model.addAttribute("colors", colors);
+
         return "home";
     }
 
     @RequestMapping("/profile/{userId}")
     @ResponseBody
-    public String profile(@PathVariable int userId){
-        return String.format("profile page of %d",userId);
+    public String profile(@PathVariable int userId) {
+        return String.format("profile page of %d", userId);
     }
 
 }
---
+++ src/main/java/com/project/model/User.java
@@ -0,0 +1,27 @@
+package com.project.model;
+
+/**
+ * Created by Claire on 11/25/17.
+ */
+public class User {
+
+    private String userName;
+    private long userId;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
+
